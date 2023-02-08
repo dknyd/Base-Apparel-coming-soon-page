430d13ff888c460499333a647481b3fe51b05114
trezmor <104760231+trezmor@users.noreply.github.com>
2023-02-08 10:58:28 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// function activeState() {
+// 	const emailContainer = document.getElementById("icon");
+// 	const newDiv = document.createElement("IMG");
+// 	newDiv.src = "images/icon-error.svg";
+// 	emailContainer.appendChild(newDiv);
+// }
+
+function activeState(){
+	if (inputContainer.children.length <4){
+		const inputContainer = document.getElementById("inputContainer");
+	const newDiv = document.createElement("IMG");
+	newDiv.src = "images/icon-error.svg";
+	inputContainer.appendChild(newDiv);
+	}
+}
